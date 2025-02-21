3f9668ba30576ce861ae47cff06bc3eb8111da61
TocaDoCoelho <144127648+RaffaelCoelho@users.noreply.github.com>
2025-02-21 11:12:18 -0300

    produtocontrole.js

--- produtocontrole.js
+++ produtocontrole.js
@@ -3,19 +3,23 @@ package br.com.example.produtos.controle;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.example.produtos.modelo.ProdutoModelo;
+import br.com.example.produtos.modelo.RespostaModelo;
 import br.com.example.produtos.servico.ProdutoServico;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+
 
 
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ProdutoControle {
     @Autowired
     private ProdutoServico ps;
@@ -36,6 +40,10 @@ public class ProdutoControle {
         return "api de produtos funcionando!";
     }
 
+  @DeleteMapping("/remover/{codigo}")
+  public ResponseEntity<RespostaModelo> remover(@PathVariable long codigo) {
+      return ps.remover(codigo);
+  }
   
 
 }
